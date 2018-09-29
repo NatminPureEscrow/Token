4d41c635e66ed68251cef570764d5140ef70b554
Natmin Pure Escrow <39843150+natminpe@users.noreply.github.com>
2018-09-29 14:38:54 +1000

    Add files via upload

--- contracts/NatminToken.sol
+++ contracts/NatminToken.sol
@@ -89,11 +89,7 @@ contract ERC223Standard {
 
 //ERC223 function to handle incoming token transfers
 contract ERC223ReceivingContract { 
-    function tokenFallback(address _from, uint256 _value, bytes _data) public pure {
-        _from;
-        _value;
-        _data;
-    }
+    function tokenFallback(address _from, uint256 _value, bytes _data) public {}
 }
 
 contract BurnToken is Ownable {
